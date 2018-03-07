048aff9b4e48d36c5f158b8c2ad0bd2ccc352c19
krushna <krushnakant.mardikar@gslab.com>
2018-03-07 07:32:49 -0800

    Modified the q-srv.sh to start server from any directory

--- Q_HTTP/q-srv.sh
+++ Q_HTTP/q-srv.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-export LUA_PATH="$LUA_PATH;$PWD/../Q_REPL/?.lua;;"
-luajit q_server.lua $1
+export LUA_PATH="$LUA_PATH;`pwd`/?.lua;`pwd`/?/init.lua;;"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+luajit $SCRIPT_DIR/q_server.lua $1
