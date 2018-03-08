567e5250c53e06a9f89735113c58d7a593935028
krushna <krushnakant.mardikar@gslab.com>
2018-03-08 04:11:47 -0800

    Updated the QLI server start script to work from any location

--- Q_HTTP/q-srv.sh
+++ Q_HTTP/q-srv.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-export LUA_PATH="$LUA_PATH;`pwd`/?.lua;`pwd`/?/init.lua;;"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+W_DIR=`pwd`
+cd $SCRIPT_DIR/../../
+export LUA_PATH="$LUA_PATH;`pwd`/?.lua;`pwd`/?/init.lua;;"
+cd $W_DIR
 luajit $SCRIPT_DIR/q_server.lua $1
