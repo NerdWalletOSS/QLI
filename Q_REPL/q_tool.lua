74333c66f3bc6b45b4f6389e03d043d89d4248c7
krushna <krushnakant.mardikar@gslab.com>
2018-03-15 22:21:55 -0700

    Updated the QLI standalone mode to save and restore sessions if pre-defined metadata file exists, also nowonwards, os.exit() will be used to exit from QLI

--- Q_REPL/q_tool.lua
+++ Q_REPL/q_tool.lua
@@ -6,13 +6,38 @@ Usage: luajit Q/Q_REPL/q_tool.lua [<host> <port>]
 
 print '----- Welcome to Q! ------'
 
-local repl = require 'QLI/Q_REPL/start_repl'
-local eval = require 'QLI/Q_REPL/q_eval_line'
-local res_str = require 'QLI/Q_REPL/q_res_str'
+local repl	= require 'QLI/Q_REPL/start_repl'
+local eval	= require 'QLI/Q_REPL/q_eval_line'
+local res_str	= require 'QLI/Q_REPL/q_res_str'
+local plpath	= require 'pl.path'
+local plstring	= require 'pl.stringx'
 
 if (#arg == 0) then
+    local q_src_root = os.getenv("Q_SRC_ROOT")
+    local q_data_dir = os.getenv("Q_DATA_DIR")
+    local q_metadata_dir = os.getenv("Q_METADATA_DIR")
+    assert(q_src_root and q_data_dir, "Required environment variables are not set\nPlease run \"source $Q_SRC_ROOT/setup.sh -f\"")
+    assert(q_src_root and plpath.isdir(q_src_root))
+    assert(q_data_dir and plpath.isdir(q_data_dir))
     Q = require 'Q'
+    local q_consts = require 'Q/UTILS/lua/q_consts'
+    local meta_file_path = nil
+    if q_metadata_dir then
+      assert(plpath.isdir(q_data_dir))
+      meta_file_path = q_metadata_dir .. "/" .. q_consts.meta_file_name
+      -- Load the saved session if present
+      if plpath.exists(meta_file_path) then
+        Q.restore(meta_file_path)
+      end
+    end
     repl (function (line)
+        if plstring.strip(line) == "os.exit()" then
+          -- Save the state to file meta_file_path
+          if meta_file_path then
+            Q.save(meta_file_path)
+          end
+          os.exit()
+        end
         local success, results = eval(line)
         if success then
             return res_str(results)
